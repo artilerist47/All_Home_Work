81b55df2fb86c363f476db8b263542648cbfbcd5
artilerist47 <artilerist.sorok.sedmoi@yandex.ru>
2022-06-14 01:38:16 +1000

    Home_Work_11_основная работа выполнена

--- home_work_11/main_chapter_2.py
+++ home_work_11/main_chapter_2.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from utils import load_all_candidates, get_candidate_by_id, get_candidate_by_name
+from utils import load_all_candidates, get_candidate_by_id, get_candidate_by_name, get_candidates_by_skill
 
 app = Flask(__name__)
 
@@ -16,10 +16,16 @@ def page_candidate_by_id(candidate_id):
     return render_template("candidate_id.html", candidate=candidate)
 
 
-@app.route("/search/<candidate_name>")
+@app.route("/search/name/<candidate_name>")
 def page_candidate_by_name(candidate_name):
     candidates = get_candidate_by_name(candidate_name)
-    return render_template("search_candidate_name.html", candidates=candidates)
+    return render_template("search_candidate_by_name.html", candidates=candidates)
+
+
+@app.route("/search/skill/<candidate_skill>")
+def page_candidate_by_skill(candidate_skill):
+    candidates = get_candidates_by_skill(candidate_skill)
+    return render_template("search_candidate_by_skill.html", candidates=candidates)
 
 
 app.run()
